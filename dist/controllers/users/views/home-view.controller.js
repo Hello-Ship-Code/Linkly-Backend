5fe2fb6c83c18059e494c817bc2ec05f09d14baa
Peter <hello.ship.code@gmail.com>
2025-03-07 15:11:56 +0530

    update

--- dist/controllers/users/views/home-view.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.homeViewController = void 0;
-const homeViewController = (_req, res, _next) => {
-    res.render('home');
-};
-exports.homeViewController = homeViewController;
-//# sourceMappingURL=home-view.controller.js.map
--- dist/controllers/users/views/upload-view.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uploadViewController = void 0;
-const uploadViewController = async (_req, res, _next) => {
-    res.render('upload');
-};
-exports.uploadViewController = uploadViewController;
-//# sourceMappingURL=upload-view.controller.js.map
